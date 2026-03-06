a8841037a1d29ecd237204141647703f4c9f8838
yuntongw89-star <yuntongw89@gmail.com>
2026-03-06 18:51:33 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:50:53.557Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T17:51:33.127Z
+Commit: Fix CSS styling bug on mobile devices
