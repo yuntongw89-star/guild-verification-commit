12be68edb16c7a5e145025e7b2c5ba9ae3469222
yuntongw89-star <yuntongw89@gmail.com>
2026-03-06 18:51:01 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:50:51.409Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T17:51:00.989Z
+Commit: Resolve timezone issues in date handling
