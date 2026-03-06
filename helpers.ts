96f36dd8da388d6f7b3c482374eeae98c5f00619
yuntongw89-star <yuntongw89@gmail.com>
2026-03-06 18:51:38 +0100

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:51:08.064Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T17:51:38.273Z
+Commit: Fix CSS styling bug on mobile devices
