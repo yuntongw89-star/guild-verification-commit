a45d928ba1f5950c6271fdd97c4a448c51bd3332
yuntongw89-star <yuntongw89@gmail.com>
2026-03-07 11:16:11 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T10:15:55.767Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-07T10:16:10.748Z
+Commit: Resolve null pointer exception in user service
